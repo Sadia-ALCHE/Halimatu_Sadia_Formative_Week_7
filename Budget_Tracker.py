78c296b4a00145a564bc3e595d0f3594fd6eefab
Halimatu Sadia Mohammed <h.mohammed@alustudent.com>
2025-12-03 08:08:04 +0400

    Added a transaction list

--- Budget_Tracker.py
+++ Budget_Tracker.py
@@ -1,3 +1,8 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+transactions = []
+
+def input_transaction():
+    print("\nAdding your transaction")
+    date = input("Enter your transaction date(DD-MM-YYYY): ")
+    amount = input("Enter your transaction amount: ")
+
+    #For this part, we have to check if
